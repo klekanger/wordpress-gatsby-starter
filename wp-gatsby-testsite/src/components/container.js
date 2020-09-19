50c1692981486e571dfc024dfd85f833d9ea0996
Kurt Lekanger <kurt@lekanger.no>
2020-09-19 10:12:07 +0200

    CSS adjustments

--- wp-gatsby-testsite/src/components/container.js
+++ wp-gatsby-testsite/src/components/container.js
@@ -3,7 +3,7 @@ import React from "react"
 import styles from "./container.module.css"
 
 const Container = ({ children }) => {
-  return <div className={styles.container}>{children}</div>
+  return <div className={styles.root}>{children}</div>
 }
 
 export default Container
